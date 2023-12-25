e3fc401f878eb6b68d6358964d9d596a5108f385
Lucas Gomes <lucasgomesribeiroskt@hotmail.com>
2023-12-25 20:00:40 -0300

    feat: add routes and reanimated

--- App.tsx
+++ App.tsx
@@ -1,12 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Animated, { FadeIn, FadeInLeft, FadeInRight, FadeOut } from 'react-native-reanimated';
+
+
+function HomeScreen() {
+  const {navigate} = useNavigation()
+  function navega() {
+    setTimeout(() => {navigate('Login' as never)}, 6*1000)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+      <Animated.View entering={FadeInRight} exiting={FadeOut}>
+        <TouchableOpacity onPress={() => navega()}>
+          <Text style={styles.TouchableOpacity} >Navegar</Text>
+        </TouchableOpacity>
+      </Animated.View>
+      <StatusBar style="auto" />
+    </View>
+  ); 
+}
+
+function LoginScreen() {
+  const {navigate} = useNavigation()
+  function navega() {
+    setTimeout(() => {navigate('Home' as never)}, 6*1000)
+  }
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+      <TouchableOpacity onPress={() => navega()}>
+        <Text style={styles.TouchableOpacity} >Navegar</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  ); 
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen options={{header: () => null}} name="Home" component={HomeScreen}/>
+          <Stack.Screen name="Login" component={LoginScreen}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +60,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  TouchableOpacity: {
+    backgroundColor: 'tomato',
+    padding: 3,
+    borderRadius: 8
+  }
 });
