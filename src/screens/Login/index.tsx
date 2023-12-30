b1ab37b3a71bbe12b3bd60372e1ddafc0441f6dd
Lucas Gomes <lucasgomesribeiroskt@hotmail.com>
2023-12-29 23:05:08 -0300

    feat: change on screen design and add textInputs

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,22 +1,48 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, Image, SafeAreaView, StatusBar, Platform, TextInput } from "react-native";
 import Animated, { FadeInRight, FadeOut } from "react-native-reanimated";
 import { styles } from "./styles";
 
 export function Login() { 
+
+    const isAndroid = Platform.OS === "android"
+
     const {navigate} = useNavigation()
-    function navega() {
+
+    const goToMainScreen = () => {
       navigate("Home" as never)
     }
-  
+
     return (
-      <View style={styles.container}>
-        <Text>Login</Text>
-        <Animated.View entering={FadeInRight} exiting={FadeOut}>
-          <TouchableOpacity onPress={() => navega()} style={styles.mainButton}>
-            <Text style={styles.textMainButton}>Contribuir</Text>
-          </TouchableOpacity>
-        </Animated.View>
-      </View>
+      <SafeAreaView style={[styles.container, {
+        marginTop: isAndroid ? StatusBar.currentHeight : 0
+      }]}>
+        <View style={styles.appTitleView}>
+          <Image source={require('../../../assets/pet-friendly-1.png')} />
+          <Text style={styles.appTitle} allowFontScaling={false}>HelpDog</Text>
+        </View>
+        <View style={styles.contentContainer}>
+          <Animated.View entering={FadeInRight} exiting={FadeOut}>
+            <View style={{gap: 8}}>
+              <Text>E-mail</Text>
+              <TextInput
+                autoCapitalize="none"
+                placeholder="exemplo@gmail.com"
+                style={styles.textInputs}
+              />
+              <Text>Senha</Text>
+              <TextInput
+                autoCapitalize="none"
+                secureTextEntry
+                placeholder="0123456789"
+                style={styles.textInputs}
+              />
+            </View>
+            <TouchableOpacity onPress={goToMainScreen} style={styles.mainButton}>
+              <Text style={styles.textMainButton}>Entrar na Conta</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+      </SafeAreaView>
     ); 
   }
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -4,8 +4,31 @@ export const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+    },
+    appTitleView: {
+      flexDirection: "row",
+      gap: 6,
+      marginTop: 18,
+      alignSelf: "center",
+      marginBottom: 60
+    },
+    appTitle: {
+      color: "#3C184E",
+      textAlign: 'center',
+      fontFamily: 'Rubik_500Medium',
+      fontSize: 18,
+    },
+    contentContainer: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    textInputs: {
+      //alignContent: "center"
+      padding: 12,
+      borderWidth: 1,
+      borderColor: '#E8E8E8',
+      backgroundColor: '#E8E8E8',
     },
     mainButton: {
       width: 181,
@@ -14,10 +37,12 @@ export const styles = StyleSheet.create({
       paddingVertical: 12,
       paddingHorizontal: 4,
       borderRadius: 40,
-      gap: 10
+      gap: 10,
+      marginTop: 16
     },
     textMainButton: {
       textAlign: "center",
       color: '#fff',
+      fontFamily: 'Rubik_400Regular',
     }
   });
