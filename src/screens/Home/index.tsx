f41f71a5a96f769d3cb0ffbd858d529d4df0bba1
Lucas Gomes <lucasgomesribeiroskt@hotmail.com>
2024-01-02 22:17:09 -0300

    feat: config icons

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,53 +1,89 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, TouchableOpacity, Image, StatusBar, Platform, SafeAreaView, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StatusBar,
+  Platform,
+  SafeAreaView,
+  Dimensions,
+} from "react-native";
 import Animated, { FadeInRight, FadeOut } from "react-native-reanimated";
 import { styles } from "./styles";
-import DogImage from  "../../../assets/img.png";
+import DogImage from "../../../assets/img.png";
 import Logo from "../../../assets/pet-friendly-1.png";
 import PurpleLine from "../../../assets/vector-1.png";
 import { usePhoneCall } from "../../hooks/usePhoneCall";
 
-export function Home() { 
-    const isAndroid = Platform.OS === "android"
+export function Home() {
+  const isAndroid = Platform.OS === "android";
 
-    const {navigate} = useNavigation()
+  const { navigate } = useNavigation();
 
-    const goToLoginScreen = () => {
-      navigate("Login" as never)
-    }
+  const goToLoginScreen = () => {
+    navigate("Login" as never);
+  };
 
-    const dimensions = Dimensions.get('window');
-    const imageHeight = Math.round(((dimensions.width * 10) / 9) *1.07);
-    const imageWidth = dimensions.width;
+  const dimensions = Dimensions.get("window");
+  const imageHeight = Math.round(((dimensions.width * 10) / 9) * 1.07);
+  const imageWidth = dimensions.width;
 
-    const {callToNumber} = usePhoneCall()
-  
-    return (
-      <SafeAreaView style={[styles.container, {
-        marginTop: isAndroid ? StatusBar.currentHeight : 0
-      }]}>
-        <View style={styles.appTitleView}>
+  const { callToNumber } = usePhoneCall();
+
+  return (
+    <Animated.View entering={FadeInRight} exiting={FadeOut} style={{ flex: 1 }}>
+      <SafeAreaView
+        style={[
+          styles.container,
+          {
+            marginTop: isAndroid ? StatusBar.currentHeight : 0,
+          },
+        ]}
+      >
+              <View style={styles.appTitleView}>
           <Image source={Logo} />
-          <Text style={styles.appTitle} allowFontScaling={false}>HelpDog</Text>
+          <Text style={styles.appTitle} allowFontScaling={false}>
+            HelpDog
+          </Text>
         </View>
         <View style={styles.contentView}>
           <View style={styles.viewSlogan}>
-            <Image source={PurpleLine}/>
-            <Text style={styles.sloganText} allowFontScaling={false}>Ajude quem precisa de ajuda</Text>
+            <Image source={PurpleLine} />
+            <Text style={styles.sloganText} allowFontScaling={false}>
+              Ajude quem precisa de ajuda
+            </Text>
           </View>
-          <Text style={styles.textDescription} allowFontScaling={false}>Com apenas 1 real você pode me ajudar a alimentar cães de ruas.</Text>
-          <Animated.View entering={FadeInRight} exiting={FadeOut} style={styles.viewButtons}>
-            <TouchableOpacity onPress={goToLoginScreen} style={styles.mainButton}>
-              <Text style={styles.textMainButton} allowFontScaling={false}>Contribuir</Text>
+          <Text style={styles.textDescription} allowFontScaling={false}>
+            Com apenas 1 real você pode me ajudar a alimentar cães de ruas.
+          </Text>
+          <View style={styles.viewButtons}>
+            <TouchableOpacity
+              onPress={goToLoginScreen}
+              style={styles.mainButton}
+            >
+              <Text style={styles.textMainButton} allowFontScaling={false}>
+                Contribuir
+              </Text>
             </TouchableOpacity>
-          <TouchableOpacity style={{}} onPress={() => callToNumber('+552199999999')}>
-                <Text style={styles.textContactButton}>Entrar em contato</Text>
-          </TouchableOpacity>
-          </Animated.View>
+            <TouchableOpacity
+              style={{}}
+              onPress={() => callToNumber("+552199999999")}
+            >
+              <Text style={styles.textContactButton}>Entrar em contato</Text>
+            </TouchableOpacity>
+          </View>
         </View>
         <View>
-          <Image style={[styles.dogImage, { height: imageHeight, width: imageWidth, objectFit: "cover"}]} source={DogImage}/>
+          <Image
+            style={[
+              styles.dogImage,
+              { height: imageHeight, width: imageWidth, objectFit: "cover" },
+            ]}
+            source={DogImage}
+          />
         </View>
       </SafeAreaView>
-    ); 
-  }
+    </Animated.View>
+  );
+}
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -16,68 +16,68 @@ export function Login() {
     }
 
     return (
-      <SafeAreaView style={[styles.container, {
-        marginTop: isAndroid ? StatusBar.currentHeight : 0
-      }]}>
-        <View style={styles.appTitleView}>
-          <TouchableOpacity onPress={goToMainScreen}>
-            <Image style={styles.backButtonImage} source={backButton}/>
-          </TouchableOpacity>
-          <View style={styles.viewLogo}>
-            <Image source={require('../../../assets/pet-friendly-1.png')} />
-            <Text style={styles.appTitle} allowFontScaling={false}>HelpDog</Text>
+      <Animated.View entering={FadeInRight} exiting={FadeOut} style={{flex: 1}}>
+        <SafeAreaView style={[styles.container, {
+          marginTop: isAndroid ? StatusBar.currentHeight : 0
+        }]}>
+          <View style={styles.appTitleView}>
+            <TouchableOpacity onPress={goToMainScreen}>
+              <Image style={styles.backButtonImage} source={backButton}/>
+            </TouchableOpacity>
+            <View style={styles.viewLogo}>
+              <Image source={require('../../../assets/pet-friendly-1.png')} />
+              <Text style={styles.appTitle} allowFontScaling={false}>HelpDog</Text>
+            </View>
+              <View style={styles.backButtonImage}>
+              </View>
           </View>
-            <View style={styles.backButtonImage}>
-            </View>
-        </View>
-        <View style={styles.contentContainer}>
-          <Animated.View entering={FadeInRight} exiting={FadeOut}>
-            <View style={styles.viewLoginMessage}>
-              <Text style={styles.welcomeMessage}>Bem-vindo</Text>
-              <Text style={styles.loginMessage}>Faça login na sua conta</Text>
-            </View>
-            <View style={styles.viewInputs}>
-              <Text>E-mail</Text>
-              <TextInput
-                autoCapitalize="none"
-                placeholder="exemplo@gmail.com"
-                keyboardType="email-address"
-                autoComplete="email"
-                style={styles.textInputs}
-              />
-              <Text>Senha</Text>
-              <TextInput
-                autoCapitalize="none"
-                secureTextEntry
-                placeholder="*********"
-                keyboardType="default"
-                autoComplete="password"
-                style={styles.textInputs}
-              />
-            </View>
-            <View style={styles.rememberMeView}>
-              <RememberMe/>
-              <TouchableOpacity>
-                <Text style={{color: '#713FFF'}}>Esqueceu sua senha?</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.viewButtons}>
-              <TouchableOpacity onPress={goToMainScreen} style={styles.mainButton}>
-                <Text style={styles.textMainButton}>Entrar na conta</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={goToMainScreen} style={styles.AlternativeLoginButton}>
-                <Image style={styles.iconGoogle} source={require('../../../assets/icon-google.png')}/>
-                <Text style={styles.textAlternativeLoginButton}>Continue com Google</Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.viewRegister}>
-              <Text>Não tem uma conta?</Text>
-              <TouchableOpacity onPress={() => console.log('cadastro')}>
-                <Text style={styles.textRegister}>Cadastre-se</Text>
-              </TouchableOpacity>
-            </View>
-          </Animated.View>
-        </View>
-      </SafeAreaView>
+          <View style={styles.contentContainer}>
+              <View style={styles.viewLoginMessage}>
+                <Text style={styles.welcomeMessage}>Bem-vindo</Text>
+                <Text style={styles.loginMessage}>Faça login na sua conta</Text>
+              </View>
+              <View style={styles.viewInputs}>
+                <Text>E-mail</Text>
+                <TextInput
+                  autoCapitalize="none"
+                  placeholder="exemplo@gmail.com"
+                  keyboardType="email-address"
+                  autoComplete="email"
+                  style={styles.textInputs}
+                />
+                <Text>Senha</Text>
+                <TextInput
+                  autoCapitalize="none"
+                  secureTextEntry
+                  placeholder="*********"
+                  keyboardType="default"
+                  autoComplete="password"
+                  style={styles.textInputs}
+                />
+              </View>
+              <View style={styles.rememberMeView}>
+                <RememberMe/>
+                <TouchableOpacity>
+                  <Text style={{color: '#713FFF'}}>Esqueceu sua senha?</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.viewButtons}>
+                <TouchableOpacity onPress={goToMainScreen} style={styles.mainButton}>
+                  <Text style={styles.textMainButton}>Entrar na conta</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={goToMainScreen} style={styles.AlternativeLoginButton}>
+                  <Image style={styles.iconGoogle} source={require('../../../assets/icon-google.png')}/>
+                  <Text style={styles.textAlternativeLoginButton}>Continue com Google</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.viewRegister}>
+                <Text>Não tem uma conta?</Text>
+                <TouchableOpacity onPress={() => console.log('cadastro')}>
+                  <Text style={styles.textRegister}>Cadastre-se</Text>
+                </TouchableOpacity>
+              </View>
+          </View>
+        </SafeAreaView>
+      </Animated.View>
     ); 
   }
